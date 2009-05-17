cc153ee41abfff448a11eb072bcea0796b822b9d
iggarpe <iggarpe@08902c86-4189-11de-952f-710615d9d0d5>
2009-05-17 23:04:09 +0000

    - Added support for generic GPIO kernel style (UNUSED).

--- linux-2.6.24.3/include/asm-mips/gpio.h
+++ linux-2.6.24.3/include/asm-mips/gpio.h
@@ -1,6 +1,22 @@
-#ifndef __ASM_MIPS_GPIO_H
-#define __ASM_MIPS_GPIO_H
+/*
+ * Copyright (C) 2009 Ignacio Garcia Perez <iggarpe@gmail.com>
+ *
+ * Author: <iggarpe@gmail.com>
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ */
 
-#include <gpio.h>
+#ifndef __ASM_MIPS_GPIO_H__
+#define __ASM_MIPS_GPIO_H__
 
-#endif /* __ASM_MIPS_GPIO_H */
+#ifdef CONFIG_JZSOC
+#include <asm/jzgpio.h>
+#endif
+
+/* cansleep wrappers */
+#include <asm-generic/gpio.h>
+
+#endif /* __ASM_JZ4740_GPIO_H */
