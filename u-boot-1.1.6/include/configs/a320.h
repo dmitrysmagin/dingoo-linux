3403e6f8e58f845d3e328d9dea38b769a10735e2
iggarpe <iggarpe@08902c86-4189-11de-952f-710615d9d0d5>
2009-06-22 19:23:25 +0000

    - Moved original firmware chainloading to the NAND SPL.

--- u-boot-1.1.6/include/configs/a320.h
+++ u-boot-1.1.6/include/configs/a320.h
@@ -91,12 +91,11 @@
 
 #define CONFIG_ZERO_BOOTDELAY_CHECK	1	/* Allow stopping autoload even if bootdelay is zero */
 #define CONFIG_BOOTDELAY		0
-#define CONFIG_BOOTCOMMAND		"nand read 80E00000 40000 C0000; go 80E10004"
+#define CONFIG_BOOTCOMMAND		"mmcinit; fatload mmc 0 80600000 zimage; go 80600000"
 #define CONFIG_BOOTFILE	        	""
 #define CONFIG_BOOTARGS			""
 #define CFG_AUTOLOAD			"y"
 
-#define CONFIG_ALTBOOTCOMMAND		"mmcinit; fatload mmc 0 80600000 zimage; go 80600000"
 
 /*
  * Serial download configuration
