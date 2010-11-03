291fefea3ce9f4c7b8ac40dbdd1e567db06f3db3
iggarpe <iggarpe@08902c86-4189-11de-952f-710615d9d0d5>
2010-11-03 19:53:03 +0000

    - FIXED: memcpy compile issue.

--- trunk/u-boot-1.1.6/config.mk
+++ trunk/u-boot-1.1.6/config.mk
@@ -134,7 +134,7 @@ ARFLAGS = crv
 endif
 RELFLAGS= $(PLATFORM_RELFLAGS)
 DBGFLAGS= -g # -DDEBUG
-OPTFLAGS= -Os #-fomit-frame-pointer
+OPTFLAGS= -Os -mno-memcpy #-fomit-frame-pointer
 ifndef LDSCRIPT
 #LDSCRIPT := $(TOPDIR)/board/$(BOARDDIR)/u-boot.lds.debug
 ifeq ($(CONFIG_NAND_U_BOOT),y)
